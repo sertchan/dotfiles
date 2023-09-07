124190c0ae9d38e657304427c0836b187808fc74
Seyhan <ardaseyhantemli1@gmail.com>
2023-09-08 00:52:17 +0300

    Get Bluetooth Volume limitations work again

--- scripts/volume/lower-volume.sh
+++ scripts/volume/lower-volume.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if bluetoothctl info | grep -q "Icon: audio-headset"; then
-    wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%-
-    #pkill -RTMIN+8 waybar
+    wpctl set-volume -l 0.6 @DEFAULT_AUDIO_SINK@ 3%-
+    pkill -RTMIN+8 waybar
 else
     wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%-
-    #pkill -RTMIN+8 waybar
+    pkill -RTMIN+8 waybar
   fi
