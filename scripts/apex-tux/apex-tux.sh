97a4aa8d25f98e704988d7dbe72fbfb6702692ab
Seyhan <ardaseyhantemli1@gmail.com>
2023-09-08 00:41:40 +0300

    Removed Unnecesseary Code On Apex Tux Script

--- scripts/apex-tux/apex-tux.sh
+++ scripts/apex-tux/apex-tux.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-apex_tux_command="apex-tux"
-settings_file="settings.toml"
-
 main() {
-    cd "$(dirname "$0")"
-    if [ ! -e "$settings_file" ] || [ ! -r "$settings_file" ]; then
-        exit 1
-    fi
-    $apex_tux_command --config "$settings_file"
+	cd "$(dirname "$0")"
+	apex-tux
 }
-main
 
+main
